6df776e10a76543c6292b81e3c14815ed06639f6
MtszSid <mateusz.sidlo@hotmail.com>
2025-03-20 19:19:57 +0100

    Add project files.

---
+++ Uniterms/Models/Operation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniterms.Models
+{
+    public abstract class Operation : AlgorithmicAlgebraObject
+    {
+        public abstract AlgorithmicAlgebraObject Left { get; set; }
+        public abstract AlgorithmicAlgebraObject Right { get; set; }
+    }
+}
---
+++ Uniterms/Models/Uniterm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniterms.Models
+{
+    internal class Uniterm : AlgorithmicAlgebraObject
+    {
+        private string _name;
+
+        public string Name { get => _name; set => _name = value; }
+
+        public Uniterm(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
